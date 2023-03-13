c5cc3e20124f97353a6e4dc949b93c14e60d69e0
PlayMola4 <claudiu.josanu@gmail.com>
2023-03-13 13:38:29 +0100

    0.2.7

--- app/src/main/java/www/iesmurgi/guiaviajeerasmus/MainActivity.kt
+++ app/src/main/java/www/iesmurgi/guiaviajeerasmus/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         firebaseAuth = FirebaseAuth.getInstance()
         openLogin()
